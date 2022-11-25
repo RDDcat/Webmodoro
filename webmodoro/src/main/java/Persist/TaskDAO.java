bdb6865c3809ca6313a946b83f0a580bede4bafa
yukmaro <2018158018@tukorea.ac.kr>
2022-11-25 13:49:02 +0900

    [UPDATE] persist DAO 업데이트

--- webmodoro/src/main/java/Persist/TaskDAO.java
+++ webmodoro/src/main/java/Persist/TaskDAO.java
@@ -1,21 +1,69 @@
 package Persist;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import Domain.TaskVO;
+
 public class TaskDAO {
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	
+	String jdbc_driver = "com.mysql.cj.jdbc.Driver";
+	String jdbc_url = "jdbc:mysql://localhost/jspdb?allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=UTC";
+	
 
 	// insert
 	public void save() {
+		connect();
 		
 	}
 	// select
-	public void load() {
-		
+	public TaskVO load() {
+		connect();
+		return null;
+	}
+	public List<TaskVO> loadAll() {
+		connect();
+		return null;		
 	}
 	// update
 	public void update() {
+		connect();
 		
 	}
 	// delete
 	public void delete() {
+		connect();
 		
+	}
+	
+	void connect() {
+		try {
+			Class.forName(jdbc_driver);
+			conn = DriverManager.getConnection(jdbc_url, "jspbook","passwd");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	void disconnect() {
+		if(pstmt != null) {
+			try {
+				pstmt.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
